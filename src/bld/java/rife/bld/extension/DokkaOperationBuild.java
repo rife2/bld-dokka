82da957696099cbadaed34439688deacdfd5ecb4
Erik C. Thauvin <erik@thauvin.net>
2025-01-14 00:15:24 -0800

    Bumped bld to version 2.2.0

--- src/bld/java/rife/bld/extension/DokkaOperationBuild.java
+++ src/bld/java/rife/bld/extension/DokkaOperationBuild.java
@@ -50,7 +50,7 @@ public class DokkaOperationBuild extends Project {
                 .include(dependency("org.jetbrains.dokka", "javadoc-plugin", dokka))
                 .include(dependency("org.jetbrains.dokka", "gfm-plugin", dokka))
                 .include(dependency("org.jetbrains.dokka", "jekyll-plugin", dokka))
-                .include(dependency("com.uwyn.rife2", "bld", version(2, 1, 0)));
+                .include(dependency("com.uwyn.rife2", "bld", version(2, 2, 0)));
         scope(test)
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 4)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 4)))
