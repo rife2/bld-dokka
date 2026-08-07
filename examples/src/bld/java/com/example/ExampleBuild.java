e41c4a3d8e3927e92cb87f24d01ce04fd3c0a6a1
Erik C. Thauvin <erik@thauvin.net>
2026-08-07 11:18:06 -0700

    Bump JUnit to version 6.1.3

--- examples/src/bld/java/com/example/ExampleBuild.java
+++ examples/src/bld/java/com/example/ExampleBuild.java
@@ -37,7 +37,7 @@ public class ExampleBuild extends Project {
         repositories = List.of(MAVEN_CENTRAL, CENTRAL_SNAPSHOTS, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
         final var kotlin = version(2, 4, 10);
-        final var junit = version(6, 1, 2);
+        final var junit = version(6, 1, 3);
         scope(compile)
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin));
         scope(test)
--- src/bld/java/rife/bld/extension/DokkaOperationBuild.java
+++ src/bld/java/rife/bld/extension/DokkaOperationBuild.java
@@ -45,7 +45,7 @@ public class DokkaOperationBuild extends Project {
         repositories = List.of(MAVEN_CENTRAL, CENTRAL_SNAPSHOTS, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
         var dokka = version(2, 2, 0);
-        var junit = version(6, 1, 2);
+        var junit = version(6, 1, 3);
         scope(compile)
                 .include(dependency("org.jetbrains.dokka", "dokka-cli", dokka))
                 .include(dependency("org.jetbrains.dokka", "dokka-base", dokka))
